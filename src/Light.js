0cd624c1863c9f48e8c24b1d5e61f81aa7eff150
Radhika Morabia <radhika.morabia@l-tron.com>
2018-11-24 22:17:51 -0500

    Early stages of app

---
+++ src/Light.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Light.css'
+
+class Light extends Component {
+  render () {
+    return (
+      <div className='box'>
+        <div className='light'>
+          cool shit
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Light
